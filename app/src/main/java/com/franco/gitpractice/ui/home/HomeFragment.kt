f3ba960a9d2181fc6241b8229585e76520473a4c
franco <franquicidad@gmail.com>
2020-11-17 19:01:28 -0500

    Prog add Text

--- app/src/main/java/com/franco/gitpractice/ui/home/HomeFragment.kt
+++ app/src/main/java/com/franco/gitpractice/ui/home/HomeFragment.kt
@@ -1,9 +1,13 @@
 package com.franco.gitpractice.ui.home
 
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -23,12 +27,24 @@ class HomeFragment : Fragment() {
                 ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
+        val button1: Button = root.findViewById(R.id.button1)
 
-        textView.text = getString(R.string.workwithAndroid)
-        textView.textSize = 24f
+        with(textView) {
+            text = getString(R.string.workwithAndroid)
+            textSize = 24f
+        }
+
+        button1.setOnClickListener {
+            textView.text = "kotlin basic"
+        }
+
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
     }
 }
+
+
+
+
